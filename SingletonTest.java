78318c854db5f1ecd24fa00a95a36d48d2b640ca
Yuchen <u7663065@anu.edu.au>
2024-03-10 10:48:42 +1100

    Singleton_design_pattern

---
+++ SingletonTest.java
@@ -0,0 +1,9 @@
+public class SingletonTest {
+    public static void main(String[] args) {
+        // We cannot instantiate it!
+        //SingletonConnection db = new SingletonConnection();
+        SingletonConnection.getInstance();
+        SingletonConnection.getInstance();
+        SingletonConnection.getInstance();
+    }
+}
